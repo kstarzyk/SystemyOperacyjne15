d0ee5bf2893d1116f4c716a4d28fa27fbc1fa29a
Krzysztof Starzyk <krzysztofstarzyk@Admins-MacBook-Pro.local>
2015-11-29 20:45:06 +0100

    Init commit second project

---
+++ pracownia2/README.md
@@ -0,0 +1,11 @@
+# SystemyOperacyjne15
+
+## Pracownia 2
+
+Program rozwiązuje problem ucztujących filozfów (https://en.wikipedia.org/wiki/Dining_philosophers_problem) za pomocą semaforów.
+
+Problem można zmodyfikować z jego oryginalnej wersji (N:= 5) na wersje z dowolnym N zmieniając wartość N i:
+a) dodając nowe etykiety do tabeli names
+b) zmieniając wszystkie wystąpienia names[pid] na pid (zamiast etykiet będą używane wtedy idiki)
+
+Napisane w C z użyciem biblioteki pthread.
---
+++ pracownia2/pracownia.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <sched.h>
+#include <ctype.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>
+
+/*== CONSTANTS ==*/
+#define N 5
+#define MAX_THINK_TIME 100000
+#define MAX_EAT_TIME  100000
+
+/*== MACROS ==*/
+#define wait sem_wait
+#define post sem_post
+#define init sem_init
+#define join pthread_join
+#define create pthread_create
+#define iter for(i=0;i<N;i++)
+
+
+/*== GLOBALS ==*/
+char *names[N] = { "Arystoteles", "Platon", "Kant", "Marks", "Nietzsche"};
+sem_t forks[N];
+pthread_t philosopherThread[N];
+
+
+
+void think(int pid)
+{
+    fprintf(stdout, "%s is thinking...\n", names[pid]);
+    usleep(rand() % MAX_THINK_TIME);
+}
+
+
+void eat(int pid)
+{
+    fprintf(stdout, "%s is eating...\n", names[pid]);
+    usleep(rand() % MAX_EAT_TIME);
+}
+
+void *_2bOrNot2b(void *arg)
+{
+    int pid = *((int *)(arg));
+    int fork;
+
+    while (1) // Filozofowie biesiadują w nieskończoność
+    {
+        think(pid);
+
+        fork = pid;
+        wait(&forks[fork]);
+        fprintf(stdout, "\t%s pick fork %d\n", names[pid], fork);
+        fork = (pid+1) % N;
+        wait(&forks[fork]);
+        fprintf(stdout, "\t%s pick fork %d\n", names[pid], fork);
+
+        eat(pid);
+
+        fork = (pid+1) % N;
+        post(&forks[fork]);
+        fprintf(stdout, "\t%s released fork %d\n", names[pid], fork);
+        fork = pid;
+        post(&forks[fork]);
+        fprintf(stdout, "\t%s released fork %d\n", names[pid], fork);
+
+    }
+
+   return NULL;
+}
+
+int main(int argc, char **argv)
+{
+
+    int i;
+    iter
+        if (init(&forks[i], 0, 1))
+            perror("ERROR: semaphore cannot be initialized");
+
+    int arg[N];
+    iter
+    {
+        arg[i] = i;
+        if (create(&philosopherThread[i], NULL, _2bOrNot2b, &arg[i]))
+            perror("ERROR: thread cannot be initialized");
+    }
+
+    iter
+        join(philosopherThread[i], NULL);
+
+    fprintf(stdout, "Dinner is done\n");
+    return EXIT_SUCCESS;
+}
+
